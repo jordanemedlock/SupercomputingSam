6f279012849ccd482c5071b2c9e28dab2604f280
samgervais <samgervais512@gmail.com>
2015-05-31 13:55:56 -0600

    It doesn't work!

--- Agario/Types/Fare.hs
+++ Agario/Types/Fare.hs
@@ -3,7 +3,11 @@
 module Types.Fare where
 
 	import Types.Cells -- Whelp... this doesn't work :'( This is frustrating 
-	
+					   -- And when Jordan explains why it's not working, he's gonna be
+					   -- like, "oh, your flux capacitor intersected with the quantum infrastructure
+					   -- of the Afro-Eurasia bose-einstein condensate which makes your list fail to 
+					   -- comply with the benevolent camaraderie between the program, file, and ghc."
+
 	data Fare = Fare { cells :: [Cell] } deriving (Show, Eq)
 
 	main :: IO()
