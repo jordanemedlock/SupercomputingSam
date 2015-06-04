d46aa9c50dd769c53bc0fb8ee943afac3e30afac
samgervais <samgervais512@gmail.com>
2015-06-03 18:25:53 -0600

    Added getXY, getX, getY, greaterMass, absorb functions. Added comments.

--- Agario/src/Types/Cells.hs
+++ Agario/src/Types/Cells.hs
@@ -1,4 +1,3 @@
--- These are the the player cells 
 module Types.Cells where
 
 type Position = (Double, Double)
@@ -12,50 +11,82 @@ data Cell = Cell { name :: String
                  , pos :: Position 
                  } deriving (Show, Eq)
 
-radius :: Cell -> Double
+-- | Finds the radius of a circular Cell using its mass as the Area by finding
+-- | the square root of the area over pi 
+radius :: Cell -- ^ contains the data of a cell to find the
+       -> Double -- ^ radius as a double
 radius (Cell { mass = m }) = sqrt(fromIntegral m / pi)
 
-distance :: Position -> Position -> Double
+-- | Calculates the distance between two points by taking taking the square   
+-- | root of the the sum of the change inthe x coordinate squared and the 
+-- | change in the y coordinate squared
+distance :: Position -- ^ position type with (x,y) as two doubles 
+         -> Position 
+         -> Double -- ^ distance between points as a double
 distance (x,y) (x2,y2) = sqrt((x2-x)**2 + (y2-y)**2)
 
-distanceBtwnCells :: Cell -> Cell -> Double
+-- | Uses the "distance" function to find the possiton between two cells
+distanceBtwnCells :: Cell -- ^ uses the positon in the cell's data 
+                  -> Cell 
+                  -> Double -- ^ distance between two cells as a double
 distanceBtwnCells c1 c2 = pos c1 `distance` pos c2 
 
-intersect :: Cell -> Cell -> Bool
+-- | Tests if two cells are intersecting by finding the distance between the
+-- | cells and testing if that is less than the sum of their radii
+intersect :: Cell -- ^ uses cell's position and mass data 
+          -> Cell 
+          -> Bool -- ^ intersection as a True or False value 
 intersect cell1 cell2 = cell1 `distanceBtwnCells` cell2 < radius cell1 + radius cell2 
 
-threshold = 10
-upperSpeed = 10
-lowerSpeedScale = 0.1
+-- Finds the x or the y coordinate of every cell in a list
+getX, getY :: Cells 
+           -> [Double] -- ^ x or y coordinate as a list of doubles
+getX cells = [fst (pos cell) | cell <- cells]
+getY cells = [snd (pos cell) | cell <- cells]
 
+-- I didn't know which of these I would need/want more... so I made them both
 
--- collide :: Cell -> Cell -> (Maybe Cell, Maybe Cell)
--- I would recommend a type signature like this so that you can 
--- differentiate which cell was absorbed into which if any were absorbed at all
--- Remember that Maybes can have a value of either (Just x) where x is a Cell or 
--- Nothing without a value
--- Of course if you want something else that is totally possible
--- Theres more than one way to do anything 
+-- | Finds the x and the y cordinate of every cell in a list
+getXY :: Cells 
+      -> [Position] -- ^ x and y as a list of the "Positiion" type
+getXY cells = [pos cell | cell <- cells] 
 
--- getX, getY, collide, checkCollisions :: Cells -> Cells
+-- | Finds and returns a cell with the mass between two cells
+greaterMass :: Cell -- ^ uses the mass of two cells
+            -> Cell 
+            -> Cell -- ^ larger cell as a cell
+greaterMass cell1 cell2 = if mass cell1 > mass cell2 then cell1 else cell2
 
--- listComprehension = [ x * y | x <- xs, y <- ys, x /= y]
--- Means multiply all values in xs by all other values in ys as long as the two 
--- values are not equal
+-- | Sees if two cells are intersecting or not, and if they are, then makes the 
+-- | smaller cell Nothing and gives the larger cell the smaller's mass
+absorb :: Cell -- ^ uses cells mass, and the "intersect" function 
+       -> Cell 
+       -> (Maybe Cell, Maybe Cell) -- ^ absorbed cell as a tuple with Just cell or Nothing
+absorb cell1 cell2 
+    | inters && mass cell1 > mass cell2 = (Just cell1 {mass = newMass}, Nothing)
+        -- returns Just cell1 with the newMass when cell1 is larger and they intersect 
+    | inters && mass cell1 < mass cell2 = (Nothing, Just cell2 {mass = newMass})
+        -- Does the same as above except using cell2 instead 
+    | otherwise                         = (Just cell1, Just cell2)
+        -- returns the two cells as Just cells when they don't intersect
+    where inters = cell1 `intersect` cell2
+          newMass = mass cell1 + mass cell2  
 
--- Let me know if theres something I missed.  Good luck 
-
-
--- | Comments look like this, if you want more information lookup `haddock` 
--- comments
-move :: Position -> Cell -> Cell
-move cursor@(curX, curY) cell = cell { pos = (x + dx, y + dy)} 
-  where l = cursor `distance` (pos cell)
+-- | Uses the position of a mouse cursor to move the cell  
+move :: Position -- ^ Position of the mouse cursor
+     -> Cell -- ^ uses position of the cell
+     -> Cell -- ^ creates a new cell with a position moving towards the cursor 
+move cursor@(curX, curY) cell = cell { pos = (x + dx, y + dy) }
+    -- finds the point the cell need to move trough crazy trigonometry
+  where l = cursor `distance` pos cell
         progress = if l > threshold then upperSpeed else l*lowerSpeedScale
         (x,y) = pos cell
         theta = atan ((y - curY) / (x - curX))
         dx = cos theta * progress
         dy = sin theta * progress
+        threshold = 10
+        upperSpeed = 10
+        lowerSpeedScale = 0.1
 
 main :: IO()
 main = do
@@ -70,6 +101,6 @@ main = do
                    , deaths = 0
                    , mass = 10
                    , pos = (0,10) 
-                 }
+                   }
   let inter = cell1 `intersect` cell2
-  print (inter, cell1 `distanceBtwnCells` cell2, (radius cell1 + radius cell2))
+  print (inter, cell1 `distanceBtwnCells` cell2, radius cell1 + radius cell2)
--- Agario/src/Types/Fare.hs
+++ Agario/src/Types/Fare.hs
@@ -2,15 +2,11 @@
 -- used by the player cell in order to gain mass
 module Types.Fare where
 
-	import Types.Cells -- Whelp... this doesn't work :'( This is frustrating 
-					   -- And when Jordan explains why it's not working, he's gonna be
-					   -- like, "oh, your flux capacitor intersected with the quantum infrastructure
-					   -- of the Afro-Eurasia bose-einstein condensate which makes your list fail to 
-					   -- comply with the benevolent camaraderie between the program, file, and ghc."
+  import Types.Cells 
 
-	data Fare = Fare { cells :: [Cell] } deriving (Show, Eq)
+  data Fare = Fare { cells :: [Cell] } deriving (Show, Eq)
 
-	main :: IO()
-	main = do
-		let food = Fare { cells=[] }
-		print food
+  main :: IO()
+    main = do
+    let food = Fare { cells=[] }
+    print food
