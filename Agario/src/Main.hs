0be828ea105bfb7b4b3f059ab77f8496e13da9d0
Jordan Medlock <jordanemedlock@gmail.com>
2015-07-13 16:33:15 -0600

    Added Fare and updated position

--- Agario/src/Main.hs
+++ Agario/src/Main.hs
@@ -1,17 +1,24 @@
 module Main where
 
 import Graphics.UI.Fungen
+import Control.Applicative
 import Types.Cells hiding (main)
+import Types.Fare hiding (main)
 import Graphics.Rendering.OpenGL (GLdouble)
+import Control.Arrow ((***))
 
-width = 400  
+width, height :: (Num a) => a
+width = 400
 height = 400
-w = fromIntegral width :: GLdouble
-h = fromIntegral height :: GLdouble
+objectManagerName = "Cells"
+
+enum :: (Enum a, Enum b) => a -> b
+enum = toEnum . fromEnum
 
 mouseHandler _ (Position x y) = do
-  gameObject <- findObject "player" "Cells"
+  gameObject <- findObject "player" objectManagerName
   (xPos, yPos) <- getObjectPosition gameObject
+  let (w,h) = (width :: GLdouble, height :: GLdouble)
   setObjectSpeed (fromIntegral (x - floor xPos) * 0.01, fromIntegral ((floor (w/2) - y) - (floor yPos - floor (h/2))) * 0.01) gameObject
   liftIOtoIOGame $ do
     print $ "(curX,curY): " ++ show (x,y)
@@ -22,15 +29,32 @@ stepAction = do
   moveAllObjects
   return ()
 
+updateCell cellObject = do
+    let cell = getGameObjectAttribute cellObject
+    cellPos <- getObjectPosition cellObject
+    let newCell = cell { pos = (realToFrac *** realToFrac) cellPos }
+    return $ updateObjectAttribute newCell cellObject
+
+updateObjectsPostitions = do
+  cells <- getObjectsFromGroup objectManagerName
+  newCells <- mapM updateCell cells
+  let group = objectGroup objectManagerName newCells
+  setObjectManagers [group]
+
 main = do
   let winConfig = ((558,116),(width,height), "Agar.IO")
-      map = colorMap 0.7 0.7 0.7  w h
-      cellPic = Basic $ Circle 10 1 0 0 Filled  
+      gameMap = colorMap 0.7 0.7 0.7  width height
+      cellPic r = Basic $ Circle (realToFrac r) 1 0 0 Filled
       cell = Cell {name="hard coded", cellNum = 0, mass = 100, pos = (0,0)}
-      player = object "player" cellPic False (w/2, h/2) (0,0) cell 
-      cells = objectGroup "Cells" [player]
-      inputHandlers = [ (MouseButton LeftButton, Press, mouseHandler)
-                       ,(Char '\27', Press, \_ _ -> funExit)
+      player = object "player" (cellPic $ radius cell) False (width/2, height/2) (0,0) cell
+      cells = objectGroup objectManagerName [player]
+      inputHandlers = [ (MouseButton LeftButton, StillDown, mouseHandler)
+                      ,(Char '\27', Press, \_ _ -> funExit)
                       ]
+  fare <- take 100 <$> generateRandomPopulation width height
+  let fareObjects = map (fareToObject cellPic) fare
+      fares = objectGroup "fares" fareObjects
+  funInit winConfig gameMap [cells, fares] 0 0 inputHandlers stepAction (Timer 30) []
 
-  funInit winConfig map [cells] 0 0 inputHandlers stepAction (Timer 30) [] 
+fareToObject :: (Double -> ObjectPicture) -> Cell -> GameObject Cell
+fareToObject picture fare = object "fare" (picture $ radius fare) False ((realToFrac *** realToFrac) (pos fare)) (0,0) fare
--- Agario/src/Types/Fare.hs
+++ Agario/src/Types/Fare.hs
@@ -1,4 +1,4 @@
--- These are the little cells that do not move, but are 
+-- These are the little cells that do not move, but are
 -- used by the player cell in order to gain mass
 module Types.Fare where
 
@@ -10,22 +10,22 @@ import qualified Data.List.Split as S
 
 data Fare = Fare { cells :: Cells } deriving (Show, Eq)
 
-generateRandomPopulation :: IO Cells
-generateRandomPopulation = do
+generateRandomPopulation :: Double -> Double -> IO Cells
+generateRandomPopulation xrange yrange = do
   let names = repeat "fare"
   let cellNums = repeat (-1)
   gen1 <- newStdGen
-  let masses = randomRs (0,100) gen2 -- make porportional 
+  let masses = randomRs (0,100) gen1
   gen2 <- newStdGen
-  let xs = randomRs (0.0,100.0) gen3 -- make porportional 
+  let xs = randomRs (0.0,xrange) gen2
   gen3 <- newStdGen
-  let ys = randomRs (0.0,100.0) gen4 -- make porportional
+  let ys = randomRs (0.0,yrange) gen3
   let positions = zip xs ys
-  return $ zipWith5 Cell names alives cellNums masses positions 
+  return $ Cell <$> names <*> cellNums <*> masses <*> positions
 
 
 main :: IO()
 main = do
-  randCells <- take 10 <$> generateRandomPopulation
-  let food = Fare { cells=randCells }  
+  randCells <- take 100 <$> generateRandomPopulation 190 100
+  let food = Fare { cells=randCells }
   print food
