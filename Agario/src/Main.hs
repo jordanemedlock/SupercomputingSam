ea5e905462d9ac23d3fb14a9fd1801eff4d6260d
Jordan Medlock <jordanemedlock@gmail.com>
2015-07-19 22:08:13 -0600

    Updated code to match FunGEn 2.0

--- Agario/src/Main.hs
+++ Agario/src/Main.hs
@@ -15,20 +15,23 @@ startCellMass = enum ((fromIntegral width+height :: Double)/8) :: Int
 maxFareMass = startCellMass `div` 2
 
 main = do
-  let winConfig = ((558,116),(width,height), "Agar.IO")
+  let winConfig = WindowConfig { wcPosition = (Point2D 558 16)
+                               , wcSize = (Point2D width height)
+                               , wcName = "Agar.IO"
+                               }
   let tile = (0, False, 0, ())
   let tiles = replicate 10 $ replicate 10 tile
   let bmpList = [("tex.bmp", Nothing)]
   let gameMap = tileMap tiles 40 40
   let cellPic radius r g b = Basic $ Circle (realToFrac radius) (realToFrac r) (realToFrac g) (realToFrac b) Filled
   let cell = Cell {name="hard coded", cellNum = 0, mass = startCellMass, pos = (0,0), rgb = (1,0,0)}
-  let player = object "player" (cellPic (radius cell) r g b) False (width/2, height/2) (0,0) cell
+  let player = object "player" (cellPic (radius cell) r g b) False (Point2D (width/2) (height/2)) (Point2D 0 0) cell
           where r = fst' $ rgb cell
                 g = snd' $ rgb cell
                 b = trd  $ rgb cell
   let cells = objectGroup cellManagerName [player]
   let inputHandlers = [ (MouseButton LeftButton, StillDown, mouseHandler)
-                      ,(Char '\27', Press, \_ _ -> funExit)
+                      , (Char '\27', Press, \_ _ -> funExit)
                       ]
   fare <- take 100 <$> generateRandomPopulation width height maxFareMass
   print $ length fare
@@ -39,9 +42,9 @@ main = do
 
 mouseHandler _ (Position x y) = do
   gameObject <- findObject "player" cellManagerName
-  (xPos, yPos) <- getObjectPosition gameObject
+  (Point2D xPos yPos) <- getObjectPosition gameObject
   let (w,h) = (width :: GLdouble, height :: GLdouble)
-  setObjectSpeed (fromIntegral (x - floor xPos) * 0.01, fromIntegral ((floor (w/2) - y) - (floor yPos - floor (h/2))) * 0.01) gameObject
+  setObjectSpeed (Point2D (fromIntegral (x - floor xPos) * 0.01) (fromIntegral ((floor (w/2) - y) - (floor yPos - floor (h/2))) * 0.01)) gameObject
   liftIOtoIOGame $ do
     print $ "(curX, curY): " ++ show (x,y)
     print $ "(objX, objY): " ++ show (xPos, yPos)
@@ -54,7 +57,7 @@ stepAction = do
 updateCell cellObject = do
     let cell = getGameObjectAttribute cellObject
     cellPos <- getObjectPosition cellObject
-    let newCell = cell { pos = (realToFrac *** realToFrac) cellPos }
+    let newCell = cell { pos = (realToFrac *** realToFrac) (xPos cellPos, yPos cellPos) }
     return $ updateObjectAttribute newCell cellObject
 
 updateObjectsPostitions = do
@@ -74,7 +77,7 @@ trd :: (a,b,c) -> c
 trd (_, _, z) = z
 
 fareToObject :: (Double -> Double -> Double -> Double -> ObjectPicture) -> Cell -> GameObject Cell
-fareToObject picture fare = object "fare" (picture (radius fare) r g b) False ((realToFrac *** realToFrac) (pos fare)) (0,0) fare
+fareToObject picture fare = object "fare" (picture (radius fare) r g b) False (Point2D (realToFrac $ fst $ pos fare) (realToFrac $ snd $ pos fare)) origin fare
     where r = fst' $ rgb fare
           g = snd' $ rgb fare
           b = trd  $ rgb fare
